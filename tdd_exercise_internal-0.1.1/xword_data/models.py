6489411d63047ffc15ab0a1354d2b6a9f9ee3f96
Striker-dot <151956367+Striker-dot@users.noreply.github.com>
2024-07-24 00:47:36 +0500

    git commit -m "Initial commit: Add Puzzle, Entry, and Clue models"

--- tdd_exercise_internal-0.1.1/xword_data/models.py
+++ tdd_exercise_internal-0.1.1/xword_data/models.py
@@ -1,3 +1,35 @@
+"""
+This module defines models for puzzles, entries, and clues in the crossword application.
+
+Models:
+- Puzzle: Represents a puzzle with a title, date, byline, and publisher.
+- Entry: Represents an entry in the crossword with unique text.
+- Clue: Represents a clue in a puzzle associated with an entry and a puzzle, with an optional theme.
+"""
+
 from django.db import models
 
-# Create your models here.
+class Puzzle(models.Model):
+    """
+    Represents a crossword puzzle with a title, date, byline, and publisher.
+    """
+    title = models.CharField(max_length=255, blank=True)
+    date = models.DateField()
+    byline = models.CharField(max_length=255)
+    publisher = models.CharField(max_length=12)
+    
+class Entry(models.Model):
+    """
+    Represents a unique entry in the crossword puzzle with text.
+    """
+    entry_text = models.CharField(max_length=50, unique=True)
+
+class Clue(models.Model):
+    """
+    Represents a clue in a puzzle, associated with an entry and a puzzle,
+    and optionally marked as a theme clue.
+    """
+    clue_text = models.CharField(max_length=512)
+    entry = models.ForeignKey(Entry, on_delete=models.CASCADE)
+    puzzle = models.ForeignKey(Puzzle, on_delete=models.CASCADE)
+    theme = models.BooleanField(default=False)                                                         
